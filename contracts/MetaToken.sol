9fca83aa1887b9f786d51a626046db79d616e220
Naman Agarwal <namana019@outlook.com>
2020-08-17 22:11:47 +0530

    Made the transfer function

--- contracts/MetaToken.sol
+++ contracts/MetaToken.sol
@@ -2,9 +2,27 @@ pragma solidity ^0.5.16;
 
 contract MetaToken {
 
+    string public name = "Meta Token";
+    string public symbol = "META";
     uint public totalSupply;
+    mapping(address => uint) public balanceOf;
 
-    constructor() public{
-        totalSupply = 1000000;
+    event Transfer(
+        address indexed _from,
+        address indexed _to,
+        uint _value
+    );
+
+    constructor(uint _initialSupply) public{
+        totalSupply = _initialSupply;
+        balanceOf[msg.sender] = _initialSupply;
+    }
+
+    function transfer(address _to, uint _value) public returns (bool success){
+        require(balanceOf[msg.sender] > _value);
+        balanceOf[msg.sender] -= _value;
+        balanceOf[_to] += _value;
+        Transfer(msg.sender,_to,_value);
+        return true;
     }
 }
