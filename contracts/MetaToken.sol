44e91369e84b6af5c175c0d42b75cbb4d91a315a
Naman Agarwal <namana019@outlook.com>
2020-08-17 23:22:39 +0530

    Added the approve function

--- contracts/MetaToken.sol
+++ contracts/MetaToken.sol
@@ -6,10 +6,17 @@ contract MetaToken {
     string public symbol = "META";
     uint public totalSupply;
     mapping(address => uint) public balanceOf;
+    mapping(address => mapping(address => uint)) public allowance;
 
     event Transfer(
         address indexed _from,
         address indexed _to,
+        uint _value
+    );
+
+    event Approval(
+        address indexed _owner,
+        address indexed _spender,
         uint _value
     );
 
@@ -23,6 +30,12 @@ contract MetaToken {
         balanceOf[msg.sender] -= _value;
         balanceOf[_to] += _value;
         Transfer(msg.sender,_to,_value);
+        return true;
+    }
+
+    function approve(address _spender, uint _value) public returns (bool success) {
+        allowance[msg.sender][_spender] = _value;
+        emit Approval(msg.sender,_spender,_value);
         return true;
     }
 }
